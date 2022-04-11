a971aac4bfb92732e9b4f407071168f6dbbd144c
Patryk Kuban <kuban457@gmail.com>
2022-04-11 21:25:21 +0200

    added forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './components/pages/HomePage'
 import FirstPage from './components/pages/FirstPage'
 import SecondPage from './components/pages/SecondPage'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import Application from './components/pages/Application'
 
 
 export default function App() {
@@ -14,6 +17,9 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/firstpage" element={<FirstPage />} />
         <Route path="/secondpage" element={<SecondPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/apply" element={<Application />} />
     </Routes>
   </BrowserRouter>
   );
